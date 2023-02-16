0243cb6ccb88994840717fa2aec39e2ddf772e6b
Pavel Shevchuk <pavashev.work@gmail.com>
2023-02-16 15:47:27 +0200

    fix i18n

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,2 +1,1 @@
 export { classNames } from './classNames/classNames';
-export { renderWithTranslation } from './tests/renderWithTranslation/renderWithTranslation';
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
-import { renderWithTranslation } from '@/shared/lib';
+import { renderWithTranslation } from '@/shared/lib/tests/renderWithTranslation/renderWithTranslation';
 
 describe('Sidebar', () => {
     test('render', () => {
